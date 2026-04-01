04010a308bd1c8a287fae55119d5e19825044877
SOUVIK MONDAL <sm6.souvik@gmail.com>
2026-04-01 06:54:54 +0000

    datatypes

---
+++ souvik_01/01_variables.js
@@ -0,0 +1,18 @@
+const accountId= 144553
+let accountEmail= "souvik.sm@gmail.com"
+var accountPassword= "12345"
+accountCity= "Kolkata"
+
+//accountId =2 // this is not allowed because its constant 
+accountEmail=" fwww.fewf@gmail.com" 
+accountPassword ="15454"
+accountCity= "Meghalaya"
+
+console.log(accountId);
+/*console.log(accountEmail);
+console.log(accountPassword);
+console.log(accountCity);*/
+console.table([accountId, accountEmail , accountPassword, accountCity])
+
+
+ 
