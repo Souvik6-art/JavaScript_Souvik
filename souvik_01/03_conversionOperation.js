70c62267dfc6fbf91c475f46d5c3bb1cf3d01e7b
Souvik Mondal <sm6.souvik@gmail.com>
2026-04-02 23:24:58 +0530

    learnt some Conversion Operation

--- souvik_01/03_conversionOperation.js
+++ souvik_01/03_conversionOperation.js
@@ -1,4 +1,4 @@
-let score= "556"
+ let score= "556"
  console.log(typeof score);
  // console.log(typeof (score));
 
@@ -19,3 +19,24 @@ let is_tall1= Boolean(is_tall);
 console.log(is_tall1)
 // 0 or empty String => false
 //1 or valued string => true
+
+let str = "souvik"
+let str1 = "Mondal"
+let fullName = str + " " + str1
+console.log(fullName)
+
+console .log(1+"1") // 11
+console.log(1+Number("1")) // 2
+console.log("1"+1) // 11
+console.log(1+ String(1)+5) // 115
+console.log(1+5+String(1)) // 61
+// Sring first me hai to sab string ke hisab se add hoga .. agar number first hai to pehle wala as number operation hoga.
+
+console.log(+true) // 1
+console.log(+false) // 0
+
+let x=12
+x++;
+console.log(x) // 13
+--x;
+console.log(x) // 12
