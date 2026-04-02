117b3e7e6daad3ba99e1a0563b235305337533da
Souvik Mondal <sm6.souvik@gmail.com>
2026-04-03 01:14:27 +0530

    learnt some Comparison Operations.. also got introduced by Strict checking

---
+++ souvik_01/04_comparison.js
@@ -0,0 +1,18 @@
+console.log(2>1);
+/*console.log(2<1);
+console.log(2>=1);
+console.log(2==1);
+*/
+
+console.log("2">1);
+    console.log(Number("2")>1);
+
+console.log(null>0); // false
+console.log(null==0); //false
+console.log(null<0); // true
+console.log(undefined>0); // false  
+console.log(null>=0); // false
+
+// === is Strict equality checker ... it checks both value and data type
+console.log(2=="2"); // true ... here data type is different but value is same so it will return true       
+console.log(2==="2"); // false becuse data type is different    
