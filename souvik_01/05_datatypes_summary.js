63da388201eb2ebd4a10a072ae77679b66aefc09
Souvik Mondal <sm6.souvik@gmail.com>
2026-04-03 22:58:01 +0530

    heap and stack uses

--- souvik_01/05_datatypes_summary.js
+++ souvik_01/05_datatypes_summary.js
@@ -45,3 +45,27 @@ function myFunction(){
  
 console.log(myFunction()) // function wo print v karega aur undefined v print karega because return data type mentained nehi kiya hai
 console.log(typeof bigInt1) // bigint
+
+
+
+//----------------------------------------------------------------------------------------------------------------------------------------
+// Stack (primitive data types) and Heap (non-primitive data types) memory allocation in JavaScript
+ // Stack
+let myYoutubeName = "Sita Ram" // primitive data type (string) stored in stack memory
+let myAnotherYoutubeName = myYoutubeName // another variable assigned with the value of myYoutubeName, also stored in stack memory
+console.log(myYoutubeName) // Sita Ram
+myAnotherYoutubeName= "Souvik.SM" // primitive data type (string) stored in stack memory
+console.log(myAnotherYoutubeName) // Souvik.SM
+ 
+//Heap
+let user_01={
+    name: "Souvik",
+    age: 23,
+ city: "Kolkata",
+    Upi: "souvik@okaxis"
+}
+//console.log(user_01)
+let user_02 ={user_01}
+user_02.Upi = "souvik@okhdfc" 
+console.log(user_01.Upi) 
+console.log(user_02.Upi)  
