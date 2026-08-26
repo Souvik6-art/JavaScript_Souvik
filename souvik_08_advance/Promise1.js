ef0c975ddd9224a1317b9b65c36e1d9d2da47af4
Souvik Mondal <sm6.souvik@gmail.com>
2026-08-27 02:48:11 +0530

    more about promise

--- souvik_08_advance/Promise1.js
+++ souvik_08_advance/Promise1.js
@@ -37,3 +37,60 @@ setTimeout (function(){
 console.log(userDetails);
 
     })
+
+let a=5;
+let b=10;
+
+    const Promisethree01 = new Promise(function(resolve, reject){
+        setTimeout(function(){
+        
+            resolve(a+b)
+        },100)
+    })
+    Promisethree01.then(
+        function(result){
+    console.log(result);
+        }
+    );
+
+    // Fourth Promise Style....
+    promisefour = new Promise(function(resolve, reject){
+setTimeout(function(){
+    let error= false;
+    if(!error){resolve({Username:"Souvik", UserEmail:"Souvik.mondal@gmail.com", userAge:"23", userPassword:"1235"})
+        }else{reject('ERROR: Something Went Wrong')}
+},1000)
+
+    })
+
+    promisefour.then((user) =>{
+        console.log(user);
+         return user.Username
+    }).then((username)=>{console.log(username);
+            }).catch(function(error){console.log(error);
+            }).finally(()=> console.log("Finally all solved") )
+
+
+
+            // Promise five ...
+            const Promisefive = new Promise(function(resolve, reject){
+               
+                setTimeout(function(){
+let error =true;
+if(!error){ resolve({Username:"Souvik", UserEmail:"Souvik.mondal@gmail.com", userAge:"23", userPassword:"1235"})}
+else{ reject('ERROR: JS went wrong')}
+                },1000)
+
+            })
+    async function consumePromiseFive() {
+      
+      try {
+        const response =  await Promisefive
+      console.log(response);
+      } catch (error) {
+        console.log(error);
+        
+      } 
+
+    }
+    consumePromiseFive()
