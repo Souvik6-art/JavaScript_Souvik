f10293a7f6de888e7ff727519c8ca64fb90bd0ba
Souvik Mondal <sm6.souvik@gmail.com>
2026-08-29 17:56:09 +0530

    fetch  api

--- souvik_08_advance/fetch/fetch.js
+++ souvik_08_advance/fetch/fetch.js
@@ -1,8 +1,23 @@
 const url = "https://api.thecatapi.com/v1/images/0XYvRd7oD"
 
-
+/*
 const getdata= async ()=> {
     let response = await fetch(url);
+    console.log(response);
     console.log(response.status);
-    
-};
+   let data = await response.json() // .json is also asynchronous and returns a promise.
+console.log(data);
+
+};  */
+const button1= document.getElementById("btn");
+const paragraph = document.querySelector("#para");
+ 
+
+function getdata (){
+    fetch(url)
+    .then((response)=> {return response.json();})
+    .then((data)=> {paragraph.innerText = JSON.stringify(data, null, 2);
+//paragraph.innerHTML =data.description;
+    });
+}
+button1.addEventListener("click", getdata);
